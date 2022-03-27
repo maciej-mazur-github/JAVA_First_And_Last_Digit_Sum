138c96a67fea5af69d29039ede8ab60f0d2c444c
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-03-27 22:40:09 +0200

    Initial commit

---
+++ src/FirstLastDigitSum.java
@@ -0,0 +1,27 @@
+public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number) {
+        if(number < 0) {
+            return -1;
+        }
+
+        int leastSignificantDigit = number % 10;
+        int mostSignificantDigit = 0;
+
+
+        do {
+            number /= 10;
+            mostSignificantDigit = number;
+
+        } while (number != 0);
+
+        return mostSignificantDigit + leastSignificantDigit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(2));
+        System.out.println(sumFirstAndLastDigit(-1));
+        System.out.println(sumFirstAndLastDigit(22));
+        System.out.println(sumFirstAndLastDigit(1345));
+        System.out.println(sumFirstAndLastDigit(25789));
+    }
+}
